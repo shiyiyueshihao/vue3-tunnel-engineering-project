6b20492035664d1fcc41cbf72b6e0a15fd28cbed
当时我自爱桐乡 <2366009203@qq.com>
2025-12-24 14:02:38 +0800

    feat: 在 dev 分支尝试修改注释

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,7 @@
 /**
  *      时间戳格式化工具
- *          ProjectInfoView.vue 的 el-table-column 的 :formatter="dataFormater" 需要四个参数  --  这里做了封装 第三个 就是我们需要处理的时间戳
+ *          ProjectInfoView.vue 的 el-table-column 的 :formatter="dataFormater" 需要四个参数
+ *              --  这里做了封装 第三个 就是我们需要处理的时间戳
 */
 export function dataFormater(row: any, column: any, timestamp: any, index: number) {
     let date = new Date(timestamp)      //  创建实例对象
