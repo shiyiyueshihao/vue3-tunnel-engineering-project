6bafd8e0b1e1bb9bb4730df88570a9162cd544a6
当时我自爱桐乡 <2366009203@qq.com>
2026-01-03 19:50:29 +0800

    fix: 优化了 echarts 的堆叠模式(线line的数据堆叠的数据)

--- server/data/line.ts
+++ server/data/line.ts
@@ -4,19 +4,16 @@ export default {
             {
                 name: '一期工程',
                 type: 'line',
-                stack: 'Total',
                 data: [120, 132, 101, 134, 90, 230, 210]
             },
             {
                 name: '二期工程',
                 type: 'line',
-                stack: 'Total',
                 data: [220, 182, 191, 234, 290, 330, 310]
             },
             {
                 name: '三期工程',
                 type: 'line',
-                stack: 'Total',
                 data: [150, 232, 201, 154, 190, 330, 410]
             }
         ]
